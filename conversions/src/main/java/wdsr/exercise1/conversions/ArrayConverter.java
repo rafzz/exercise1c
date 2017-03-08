c76da9a05fefe09070ecb4a9ce3e6b211fdfce82
rafzz <rafzz@interia.eu>
2017-03-08 18:25:20 +0100

    exe1c

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -1,12 +1,29 @@
 package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+ import org.apache.logging.log4j.Logger;
 
 /**
  * Created by Marek on 14.02.2016.
  */
+ 
+
+		
 public class ArrayConverter {
+	 
+	
+	private static final Logger logger = LogManager.getLogger();
+      
+	
     public int[] convertToInts(String[] strings) {
-        return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+		logger.debug("convertToInts, args: " + Arrays.toString(strings));
+		
+ 		int[] result = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+		
+ 		logger.debug("convertToInts, result: " + Arrays.toString(result));
+		
+ 		
+ 		return result;
     }
 }
